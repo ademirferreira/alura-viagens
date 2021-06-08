96b078b601580a579b04982d0b0b18145711b50a
Ademir Ferreira <whoisademir@gmail.com>
2021-06-07 21:40:43 -0300

    Criando o banner

---
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,16 @@
+package br.com.alura.aluraviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.alura.aluraviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}

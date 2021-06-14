ce25a6953575827760f942ba2e629c88d5484692
Ademir Ferreira <whoisademir@gmail.com>
2021-06-14 17:51:18 -0300

    Realizando o bind das informações

--- app/src/main/java/br/com/alura/aluraviagens/ui/adapter/ListaPacotesAdapter.java
+++ app/src/main/java/br/com/alura/aluraviagens/ui/adapter/ListaPacotesAdapter.java
@@ -1,10 +1,14 @@
 package br.com.alura.aluraviagens.ui.adapter;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -37,6 +41,24 @@ public class ListaPacotesAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View viewCriada = LayoutInflater.from(context).inflate(R.layout.item_pacote, parent, false);
+
+        Pacote pacote = pacotes.get(position);
+
+        TextView local = viewCriada.findViewById(R.id.item_pacote_local);
+        local.setText(pacote.getLocal());
+
+        ImageView imagem = viewCriada.findViewById(R.id.item_pacote_imagem);
+        Resources resources = context.getResources();
+        int idDrawable = resources.getIdentifier(pacote.getImagem(), "drawable", context.getPackageName());
+        Drawable drawableImagemPacote = resources.getDrawable(idDrawable);
+        imagem.setImageDrawable(drawableImagemPacote);
+
+        TextView dias = viewCriada.findViewById(R.id.item_pacote_dias);
+        dias.setText(pacote.getDias() + "dias");
+
+        TextView preco = viewCriada.findViewById(R.id.item_pacote_preco);
+        preco.setText(pacote.getPreco().toString());
+
         return viewCriada;
     }
 }

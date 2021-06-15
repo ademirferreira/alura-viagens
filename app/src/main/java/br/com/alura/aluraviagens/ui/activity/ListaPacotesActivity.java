80999b249fb40ea97bccd6575ae63783daf362bf
Ademir Ferreira <whoisademir@gmail.com>
2021-06-15 20:48:56 -0300

    Criação da tela de resumo de pacote e bind das informações

--- app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -2,6 +2,7 @@ package br.com.alura.aluraviagens.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
 
@@ -23,6 +24,10 @@ public class ListaPacotesActivity extends AppCompatActivity {
         setTitle(TITULO_APPBAR);
 
         configuraLista();
+
+        Intent intent = new Intent(this, ResumoPacoteActivity.class);
+        startActivity(intent);
+
     }
 
     private void configuraLista() {
---
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.java
@@ -0,0 +1,43 @@
+package br.com.alura.aluraviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+
+import br.com.alura.aluraviagens.R;
+import br.com.alura.aluraviagens.model.Pacote;
+import br.com.alura.aluraviagens.util.DiasUtil;
+import br.com.alura.aluraviagens.util.MoedaUtil;
+import br.com.alura.aluraviagens.util.ResourcesUtil;
+
+public class ResumoPacoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumo_pacote);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
+
+        TextView local = findViewById(R.id.resumo_pacote_local);
+        local.setText(pacoteSaoPaulo.getLocal());
+
+        ImageView imagem = findViewById(R.id.resumo_pacote_imagem);
+        Drawable drawablePacote = ResourcesUtil.devolveDrawable(this, pacoteSaoPaulo.getImagem());
+        imagem.setImageDrawable(drawablePacote);
+
+        TextView dias = findViewById(R.id.resumo_pacote_dias);
+        String diasEmTexto = DiasUtil.formataDiasEmTexto(pacoteSaoPaulo.getDias());
+        dias.setText(diasEmTexto);
+
+        TextView preco = findViewById(R.id.resumo_pacote_preco);
+        String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacoteSaoPaulo.getPreco());
+        preco.setText(moedaBrasileira);
+    }
+}
